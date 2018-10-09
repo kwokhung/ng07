9e890d1df6f260c5bde1ab6a717e13164b1f3e05
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-09 17:45:24 +0800

    init

--- src/app/components/expense/expense.component.ts
+++ src/app/components/expense/expense.component.ts
@@ -10,10 +10,23 @@ export class ExpenseComponent implements OnInit {
   @Input() logo: string;
 
   title = 'Expense Voucher Export';
+  likeCount = 0;
+  dislikeCount = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  vote(type: string) {
+    if (type === 'like') {
+      this.likeCount++;
+      alert(this.likeCount);
+    }
+    else {
+      this.dislikeCount++;
+      alert(this.dislikeCount);
+    }
   }
 
 }
