2d3ace64aed82a9c60ed5d56aaa02b20b571b205
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-10 11:27:20 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { ExpenseComponent } from './components/expense/expense.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { ExpenseComponent } from './components/expense/expense.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/expense/expense.component.ts
+++ src/app/components/expense/expense.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-expense',
@@ -9,9 +10,23 @@ export class ExpenseComponent implements OnInit {
 
   title = 'Expense Voucher Export';
 
+  searchForm: FormGroup;
+  applicationDate: FormControl;
+  applicationNo: FormControl;
+  payee: FormControl;
+
   constructor() { }
 
   ngOnInit() {
+    this.applicationDate = new FormControl();
+    this.applicationNo = new FormControl();
+    this.payee = new FormControl();
+
+    this.searchForm = new FormGroup({
+      'applicationDate': this.applicationDate,
+      'applicationNo' : this.applicationNo,
+      'payee' : this.payee
+    });
   }
   
 }
