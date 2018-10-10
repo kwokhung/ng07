befdb8fd90d5041367baf9bdbeac5071976eac9c
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-10 10:53:07 +0800

    init

--- src/app/components/expense/expense.component.ts
+++ src/app/components/expense/expense.component.ts
@@ -7,26 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExpenseComponent implements OnInit {
 
-  @Input() logo: string;
-
   title = 'Expense Voucher Export';
-  likeCount = 0;
-  dislikeCount = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  vote(type: string) {
-    if (type === 'like') {
-      this.likeCount++;
-      alert(this.likeCount);
-    }
-    else {
-      this.dislikeCount++;
-      alert(this.dislikeCount);
-    }
-  }
-
+  
 }
