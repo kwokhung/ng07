f3866d9c14b1cbc22602973dd2b790dd765cd347
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-11 16:03:52 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-//import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,7 +17,7 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { ExpenseService } from './services/expense.service';
 import { LoaderService } from './services/loader.service';
 
-library.add(faCoffee);
+library.add(faSpinner);
 
 @NgModule({
   declarations: [
@@ -33,7 +32,6 @@ library.add(faCoffee);
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    //AngularFontAwesomeModule,
     FontAwesomeModule
   ],
   providers: [ExpenseService, LoaderService],
