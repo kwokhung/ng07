540a28e1ab0b54e22f228a7a663fa58ea6078878
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-12 14:23:01 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-//import { library } from '@fortawesome/fontawesome-svg-core';
-//import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,8 +14,6 @@ import { ExportItemComponent } from './components/export-item/export-item.compon
 
 import { ExpenseService } from './services/expense.service';
 import { LoaderService } from './services/loader.service';
-
-//library.add(faSpinner);
 
 @NgModule({
   declarations: [
@@ -35,8 +29,7 @@ import { LoaderService } from './services/loader.service';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule/*,
-    FontAwesomeModule*/
+    HttpClientModule
   ],
   providers: [ExpenseService, LoaderService],
   bootstrap: [AppComponent]
