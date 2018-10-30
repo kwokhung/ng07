702db544e6fff93d532ff86026f124790f6428b4
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-30 10:03:17 +0800

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //expensesUrl: 'http://localhost:5400/api/ExpenseService'
+  //expensesUrl: 'http://localhost:5400/api/ExpenseService',
   expensesUrl: 'http://10.32.11.70:8080/api/ExpenseService',
   operationCondition: 1
 };
