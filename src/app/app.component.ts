f8cc4c2da61cad9a2bb13840b420822227d15593
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-11-07 12:01:21 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
 import { AuthenticationService } from './services/authentication.service';
@@ -10,11 +10,31 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = (environment.operationCondition == 0 ? 'Mason Group' : 'Mason Securities Limited');
 
   constructor(private router: Router, private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit() {
+    let authenticationUrl: string = localStorage.getItem("authenticationUrl");
+
+    if (authenticationUrl) {
+      environment.authenticationUrl = authenticationUrl;
+    }
+
+    let expensesUrl: string = localStorage.getItem("expensesUrl");
+
+    if (expensesUrl) {
+      environment.expensesUrl = expensesUrl;
+    }
+
+    let operationCondition: string = localStorage.getItem("operationCondition");
+
+    if (operationCondition) {
+      environment.operationCondition = +operationCondition;
+    }
   }
 
   isAuthenticated(): boolean {
