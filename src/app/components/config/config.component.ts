89fa2d099d74750bfd4c1bf906c3c946e2799c35
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-11-07 14:16:34 +0800

    init

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+
+import { AuthenticationService } from '../../services/authentication.service';
+import { LoaderService } from '../../services/loader.service';
+
+import { LoginParameter } from 'src/app/models/login-parameter';
+
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-config',
@@ -7,9 +16,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigComponent implements OnInit {
 
-  constructor() { }
+  title = 'Configuration';
+
+  loginForm: FormGroup;
+  domain: FormControl;
+  userId: FormControl;
+  password: FormControl;
+
+  invalidLogin: boolean = false;
+
+  domains = [
+    'MASONHK',
+    'D4331'
+  ];
+
+  constructor(private router: Router, private authenticationService: AuthenticationService, private loaderService: LoaderService) {
+  }
 
   ngOnInit() {
+    this.domain = new FormControl(this.domains[environment.operationCondition])
+
+    /*this.domain = new FormControl(null, [
+      Validators.required,
+      this.regExValidator(/^(MASONHK|D4331)$/i)
+    ]);*/
+
+    this.userId = new FormControl(null, Validators.required);
+
+    this.password = new FormControl(null, Validators.required);
+
+    this.loginForm = new FormGroup({
+      'domain': this.domain,
+      'userId': this.userId,
+      'password': this.password
+    });
   }
+
+  login(parameter: LoginParameter) {
+    this.loaderService.showLoader();
+
+    //await this.loaderService.delay(1000);
+
+    this.authenticationService.login(parameter)
+      .subscribe(
+        token => {
+          if (token) {
+            this.router.navigateByUrl('/');
+          }
+          else {
+            this.invalidLogin = true;
+          }
+
+          this.loaderService.hideLoader();
+        }
+      );
+  }
+
+  /*private regExValidator(regEx: RegExp): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      return (regEx.test(control.value) ? null : { 'regEx': { value: 'Field is invalid.' } });
+    };
+  }*/
 
 }
