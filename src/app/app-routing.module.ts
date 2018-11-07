01c077ccbb5f06da5ff8b346e634e42f53f9fd01
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-11-07 14:13:15 +0800

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ExportListComponent } from './components/export-list/export-list.compon
 import { DuplicateInvoiceComponent } from './components/duplicate-invoice/duplicate-invoice.component';
 import { ExpenseToBeExportedComponent } from './components/expense-to-be-exported/expense-to-be-exported.component';
 import { LoginComponent } from './components/login/login.component';
+import { ConfigComponent } from './components/config/config.component';
 
 import { AuthenticationGuard } from './guards/authentication.guard';
 
@@ -14,7 +15,8 @@ const routes: Routes = [
   { path: 'export-list', component: ExportListComponent, canActivate: [AuthenticationGuard] },
   { path: 'duplicate-invoice', component: DuplicateInvoiceComponent, canActivate: [AuthenticationGuard] },
   { path: 'expense-to-be-exported', component: ExpenseToBeExportedComponent, canActivate: [AuthenticationGuard] },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'config', component: ConfigComponent }
 ];
 
 @NgModule({
